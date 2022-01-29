dff79ab4d68949f4cbbcb02dad532126860d1f02
Robert World <64225836+sabrtraveler@users.noreply.github.com>
2022-01-29 13:46:04 -0500

    added array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,9 @@
 // TODO: Include packages needed for this application
+//installed inquirer package
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of the project?', 'What is the description of the project?', 'How do you install this project?', 'What is the usage information?', 'What is the contribution guidelines?', 'What is the test instructions?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
